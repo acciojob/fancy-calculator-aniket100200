aa7cec8bd0310ce47c7c0be0eea3c5900c7574eb
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-09 10:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+let v1=parseInt(prompt("Enter the first number"));
+let v2=parseInt(prompt("Enter the Second number"));
+
+let ans=v1*2+v2;
+
+alert("The final result is: " +ans);
